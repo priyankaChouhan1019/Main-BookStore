19411e7dcaabcdc9b367caa57d316005d448537c
webpriya <priyachouhan1019@gmail.com>
2022-02-04 03:56:51 -0800

    fetch book title,author, price

--- src/component/books/Books.jsx
+++ src/component/books/Books.jsx
@@ -1,61 +1,59 @@
 import React from 'react'
 import '../books/Books.scss'
 import book from '../../assets/book.png'
-import { getBooks} from '../../services/UserService'
+import { getBooks } from '../../services/UserService'
 
 function Books() {
 
-    const [bookArray , setBookArray] =React.useState([])
- 
+    const [books, setBooks] = React.useState([])
 
-    const getdisplayBooks=()=>{
-      getBooks().then((res)=>{
-        console.log(res)
-        setBookArray(res.data.result)
-      }).catch((err)=>{
-        console.log(err)
-      })
+    const getBookDetails = () => {
+        getBooks().then((res) => {
+            console.log(res)
+            setBooks(res.data.result)
+        }).catch((err) => {
+            console.log(err)
+        })
     }
-  
-    React.useEffect(()=>{
-      getdisplayBooks();
-    },[])
 
-  return (
-      <div className='card'>
-          <div className="book-container">
-              <img className="image" src={book}></img>
-          </div>
-            {/* <div className="text-containt">
-                <div className="title">
-                 <span id='title1'>don't me me thnk</span>
-                 <span id='title2'>by steve king</span>
-                 <div className="rating">
-                 <span id='number1'>4.5*</span>
-                 <span id='number2'>(20)</span>   
-                 </div>
-                 <div className="price-container">
-                     <span id='rs'>Rs.</span>
-                     <span id='new-price'>1500</span>
-                     <span id='old-price'>2000</span>
-                 </div>
-                </div> */}
+    React.useEffect(() => {
+        getBookDetails();
+    }, [])
 
-   <div >
-{
-          bookArray.map((item,index)=>(
-            <div>
-                bookName:{item.bookName}     
-                 author:{item.author}
-                 price:{item.price}
-              
-            </div>
-          ))
-        }
-            </div>
-      </div>
-     
-  )
+    return (
+
+
+        <div className='grid-container'>
+            {
+                books.map((item, index) => (
+                    <div className='card'>
+                        <div className="book-container">
+                            <img className="image" src={book}></img>
+                        </div>
+                        <div className="text-containt">
+                            <div className="title">
+                                <span id='title1'>{item.bookName}</span>
+                                <span id='title2'>{item.author}</span>
+                                <div className="rating">
+                                    <span id='number1'>4.5*</span>
+                                    <span id='number2'>(20)</span>
+                                </div>
+                                <div className="price-container">
+                                    <span id='rs'>Rs.</span>
+                                    <span id='new-price'>{item.price}</span>
+                                    <span id='old-price'>2000</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                ))
+            }
+        </div>
+
+
+
+
+    )
 }
 
 export default Books
--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,45 +1,26 @@
 import React from 'react'
 import '../dashboard/Dashboard.scss'
-// import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
-// import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
-// import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
-// import bookLogo from '../../assets/bookLogo.png'
-import { getBooks} from '../../services/UserService'
+import { getBooks } from '../../services/UserService'
 import Books from '../../component/books/Books';
 import HeadBar from '../../component/headbar/HeadBar';
 function Dashboard() {
-  const [bookArray , setBookArray] =React.useState([])
- 
+  const [bookArray, setBookArray] = React.useState([])
 
-  const getdisplayBooks=()=>{
-    getBooks().then((res)=>{
-      console.log(res)
-      setBookArray(res.data.result)
-    }).catch((err)=>{
-      console.log(err)
-    })
-  }
 
-  React.useEffect(()=>{
-    getdisplayBooks();
-  },[])
 
   return (
     <div className="dash-container">
-      <HeadBar/>
-       
-        <div className='bookList'>
-       
-        {
-          bookArray.map((item,index)=>(
-            <div>
-               <Books/>
-            </div>
-          ))
-        }
-        </div>
+      <HeadBar />
+
+    
       
-    </div>
+      <div className='bookList'>
+        <div>
+          <Books />
+        </div>
+      </div>
+
+    </div >
   )
 }
 
